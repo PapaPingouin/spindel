1c9fdd1bd1ca18049d53e8dde2b97adae263f0ab
nico <papapingouin@imw.fr>
2015-11-09 16:34:31 +0100

    1

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # spindel
+
+
+runner
+======
+(N instances)
+nodejs - ouverture port 2908 , attente connexion socketio
+première connection renvoi l'etat de la machine (hardware info) et l'etat des process si existants
+une fois connecté, heartbeat toutes les minutes
+attend de recevoir les ordres et lance les taches
+
+
+dispatcher
+==========
+nodejs - ouvrture port 2909 (connection depuis le commander)
+se connecte aux runners, et maintient la connection.
+Il connait les ip des runners, leurs status courant, et leurs capacité
+il maintient aussi les actions en cours
+
+
+commander
+=========
+envoi les demandes de traitement au dispatcher
+monitore le dispatcher (et donc les runners et les processus)
+
+
+stocker
+=======
+webservice de stockage de données (mariadb/mongo/elastic)
+
+
+finder
+======
+webservice de recherche de données (elastic)
---
+++ runner/runner.js
@@ -0,0 +1,82 @@
+var http = require('http');
+var fs = require('fs');
+
+
+
+Date.prototype.toYMD = function()
+{
+	var t = new Date( this.getTime()-this.getTimezoneOffset()*60*1000 );
+	var iso = t.toISOString().match(/(\d{4}\-\d{2}\-\d{2})T(\d{2}:\d{2}:\d{2})/)
+	return(iso[1]);
+}
+Date.prototype.toHIS = function()
+{
+	var t = new Date( this.getTime()-this.getTimezoneOffset()*60*1000 );
+	var iso = t.toISOString().match(/(\d{4}\-\d{2}\-\d{2})T(\d{2}:\d{2}:\d{2})/)
+	return(iso[2]);
+}
+Date.prototype.toYMDHIS = function()
+{
+	var t = new Date( this.getTime()-this.getTimezoneOffset()*60*1000 );
+	var iso = t.toISOString().match(/(\d{4}\-\d{2}\-\d{2})T(\d{2}:\d{2}:\d{2})/)
+	return(iso[1] + ' ' + iso[2]);
+}
+
+
+
+
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function(req, res) {
+	//console.log( req );
+    fs.readFile('../client'+req.url, function(error, content) {
+        //res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket, pseudo) {
+    // Quand on client se connecte, on lui envoie un message
+    socket.emit('proto', 'OK');
+    
+    socket.on('proto', function(request) {
+		console.log( "[runner] receive [proto] : "+request);
+		if( request == 'test' )
+			socket.emit('proto', 'OK');
+    });
+    
+    socket.on('log', function(request) {
+		console.log( "[runner] receive [log] : "+request);
+		socket.broadcast.emit( 'log', request );
+	});
+    
+    
+    socket.on('message', function (message) {
+        // On récupère le pseudo de celui qui a cliqué dans les variables de session
+        socket.get('pseudo', function (error, pseudo) {
+            console.log('[runner] '+pseudo + ' me parle ! Il me dit : ' + message);
+        });
+    }); 
+    
+    socket.on('push', function (request) {
+        // On récupère le pseudo de celui qui a cliqué dans les variables de session
+        //console.log( "push : "+request);
+        writeData( request );
+        for( e in request )
+			console.log( '[runner] push '+e+' : '+request[e] );
+    }); 
+    
+    socket.on('save', function (request) { // save la BDD complete
+        // On récupère le pseudo de celui qui a cliqué dans les variables de session
+        //console.log( "push : "+request);
+        saveBdd( request );
+        console.log( "[runner] Save BDD OK" );
+    }); 
+});
+console.log( '[runner] Demarrage serveur web');
+server.listen(2908);
+console.log( '[runner] OK' );
+
+
+
